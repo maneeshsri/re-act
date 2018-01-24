75ee70b9602ac37fbf9f4bb9d90c2fd5e2eba81f
Maneesh Srivastava <maneesh81@gmail.com>
2018-01-24 12:59:21 +0000

    More changes

--- src/config.js
+++ src/config.js
@@ -2,6 +2,18 @@
 
 import Firebase from 'firebase';
 
+
+
+ let config = {
+    apiKey: "AIzaSyDW6LVTZYmOizlsn1K5p8_tYIdHKXXcSv8",
+    authDomain: "re-act-215f9.firebaseapp.com",
+    databaseURL: "https://re-act-215f9.firebaseio.com",
+    projectId: "re-act-215f9",
+    storageBucket: "",
+    messagingSenderId: "1078341914606"
+  };
+
+/**
 let config = {
   apiKey: "AIzaSyCnxuLX6AgMduDMLtSJVDNJhR8xuMNvs4Y",
   authDomain: "tamiat-demo.firebaseapp.com",
@@ -10,7 +22,7 @@ let config = {
   storageBucket: "gs://tamiat-demo.appspot.com/",
   messagingSenderId: "188459960333"
 };
-
+***/
 let app = Firebase.initializeApp(config);
 let db = app.database();
 
